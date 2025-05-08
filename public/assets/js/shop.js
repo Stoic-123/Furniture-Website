e0bb245777236b0efe010b4df7e5c54c94e55d2e
Stoic-123 <longbigboss123@gmail.com>
2025-05-08 16:27:45 +0700

    modify

---
+++ public/assets/js/shop.js
@@ -0,0 +1,26 @@
+const listSelect = document.querySelectorAll(".item-select-main");
+const childListWrapper = document.querySelectorAll(".small-selct-pro-wrapper");
+const iconSelect = document.querySelectorAll(".select-shop-icon")
+
+listSelect.forEach((list, index) => {
+    list.addEventListener("click", () => {
+        const wrapper = childListWrapper[index];
+        wrapper.classList.toggle("active");
+        const iconSelectChange = iconSelect[index];
+        iconSelectChange.classList.toggle("active")
+    });
+});
+
+function showTabShopList(tabId, event) {
+    document.querySelectorAll('.tab-shop-content').forEach(el => {
+        el.classList.remove('active');
+    });
+
+    document.querySelectorAll('.tab-shop-button').forEach(btn => {
+        btn.classList.remove('active');
+    });
+
+    document.getElementById(tabId).classList.add('active');
+
+    event.currentTarget.classList.add('active');
+}
