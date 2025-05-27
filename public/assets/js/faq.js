7b68502eaccfacc4a1f445cdc49f34c612fd692f
Stoic-123 <longbigboss123@gmail.com>
2025-05-27 17:11:21 +0700

    fix

--- public/assets/js/faq.js
+++ public/assets/js/faq.js
@@ -24,3 +24,22 @@ function showTabShopList(tabId, event) {
 
     event.currentTarget.classList.add('active');
 }
+window.onload = () => {
+    const searchContent = document.getElementById("search-header");
+    const icon = document.getElementById("search-icon-head");
+    const cancelIcon = document.querySelector(".cancel_icon_header");
+    const body = document.body;
+    searchContent.style.marginTop = "-300px";
+    body.style.overflowY = "auto";
+    cancelIcon.addEventListener("click", () => {
+        searchContent.style.marginTop = "-300px";
+        searchContent.style.transition = "0.3s"
+        body.style.overflowY = "auto";
+    });
+    icon.addEventListener("click", () => {
+        searchContent.style.marginTop = "-45px";
+        searchContent.style.transition = "0.3s"
+        body.style.overflowY = "hidden";
+
+    });
+};
