dc9f2da6bd0e15e56c6bcd48b32c11d8c37182d4
Stoic-123 <longbigboss123@gmail.com>
2025-05-30 12:36:57 +0700

    modify

---
+++ public/assets/js/compare.js
@@ -0,0 +1,26 @@
+window.onload = () => {
+    const searchContent = document.getElementById("search-header");
+    const icon = document.getElementById("search-icon-head");
+    const icon2 = document.getElementById("search-icon-bottom");
+    const cancelIcon = document.querySelector(".cancel_icon_header");
+    const body = document.body;
+    searchContent.style.marginTop = "-300px";
+    body.style.overflowY = "auto";
+    cancelIcon.addEventListener("click", () => {
+        searchContent.style.marginTop = "-300px";
+        searchContent.style.transition = "0.3s"
+        body.style.overflowY = "auto";
+    });
+    icon.addEventListener("click", () => {
+        searchContent.style.marginTop = "-45px";
+        searchContent.style.transition = "0.3s"
+        body.style.overflowY = "hidden";
+
+    });
+    icon2.addEventListener("click", () => {
+        searchContent.style.marginTop = "-45px";
+        searchContent.style.transition = "0.3s"
+        body.style.overflowY = "hidden";
+
+    });
+};
